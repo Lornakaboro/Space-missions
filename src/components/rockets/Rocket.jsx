7df9fe798e592251a030ddff0a7d5a61fd921b8b
Lornakaboro <lornakaboro@gmail.com>
2023-04-12 09:56:55 +0300

    Fetch data from API and render it on the UI

--- src/components/rockets/Rocket.jsx
+++ src/components/rockets/Rocket.jsx
@@ -1,41 +1,33 @@
-/* eslint-disable  camelcase */
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchRockets, reserve } from '../../redux/rockets/rocketsSlice';
 
-const Rocket = ({ rocket }) => {
-  const {
-    id, name, description, flickr_images,
-  } = rocket; // eslint-disable
-  const reservationStatus = false;
+function Rockets() {
+  const { rockets, status } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!status) dispatch(fetchRockets({ method: 'GET' }));
+  }, [status, dispatch]);
 
   return (
     <div className="rocket-item">
-      <img src={flickr_images[0]} alt="Rocket item" />
-      <div className="rocket-info">
-        <h4 className="rocket-name">{name}</h4>
-        <p className="rocket-decription">{description}</p>
-        <div className="reservation-actions" data-target={id}>
-          {reservationStatus ? (
-            <button className="cancel" type="button">
-              Cancel Reservation
-            </button>
-          ) : (
-            <button className="reserve" type="button">
-              Reserve Rocket
-            </button>
-          )}
+      {rockets.map((rocket) => (
+        <div key={rocket.id} className="rocket-info">
+          <img src={rocket.image} alt={rocket.name} />
+
+          <div className="rocket-info">
+            <h2>{rocket.name}</h2>
+            <p>
+              {rocket.reserved && <span>reserved</span>}
+              {' '}
+              {rocket.description}
+            </p>
+            <button className={`${rocket.reserved ? 'cancel' : 'reserve'}`} onClick={() => { dispatch(reserve(rocket.id)); }} type="button">{`${rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}`}</button>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
-};
+}
 
-Rocket.propTypes = {
-  rocket: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    flickr_images: PropTypes.arrayOf(PropTypes.string).isRequired,
-  }).isRequired,
-};
-
-export default Rocket;
+export default Rockets;
