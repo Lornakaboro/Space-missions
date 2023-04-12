fd99eca16278c63e2d2bbc23d7adc28e9443f4f0
Lornakaboro <lornakaboro@gmail.com>
2023-04-12 18:41:46 +0300

    Display reserved rockets in my profile

---
+++ src/components/profiles/myRockets.js
@@ -0,0 +1,31 @@
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+
+function MyRockets() {
+  const reservations = useSelector((state) => state.rockets).rockets
+    .filter((rocket) => rocket.reserved);
+
+  return (
+    <div>
+      <h1 className="head-text">My Rockets</h1>
+      <table className="tour_list">
+        <tbody className="tour_body">
+          {
+              reservations.length < 1 ? (
+                <tr>
+                  <td className="tour-now tour">No Rockets Yet...</td>
+                  <td><NavLink to="/">Join a Rockets</NavLink></td>
+                </tr>
+              ) : (
+                reservations.map(({ id, name }) => (
+                  <tr className="tour" key={id}><td>{name}</td></tr>
+                ))
+              )
+            }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default MyRockets;
